383a0b55c71609a869790beb58e245a199e8464e
bkeener <bkeener7@gmail.com>
2022-10-31 11:56:45 -0600

    Refactor basket controller

--- app/controllers/customer_basket_controller.rb
+++ app/controllers/customer_basket_controller.rb
@@ -1,7 +1,6 @@
 class CustomerBasketController < ApplicationController
   def add_customer
-    item = Item.find(params[:item_id])
-    CustomerBasket.create!(item_id: item.id, customer_id: params[:customer_id])
+    CustomerBasket.create!(item_id: params[:item_id], customer_id: params[:customer_id])
     redirect_to "/customers/#{params[:customer_id]}"
   end
 end
